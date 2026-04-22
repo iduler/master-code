f6f0051b03479d0a1eba8959f1ed47679c936145
iduler <idun.lervik@live.no>
2026-04-22 21:31:45 +0200

    Rename

--- src/test_injection_heterogenious.py
+++ src/test_injection_heterogenious.py
@@ -46,21 +46,10 @@ def test_heterogeneous_parameters(parameter_method_name, boundary, expected_valu
         # Check if the values are close enough.
         assert np.allclose(model_values, expected_values)
 
-model = test_model()
-parameters_solid_1 = model.params["material_constants"]["solid"]
+
+def test_injection_flux():
+    ...
 
 
-
-#boundary = 3500 
-#test_heterogeneous_parameters("solid_density", boundry, 2400.0, 2680.0)
-#test_heterogeneous_parameters("biot_coefficient", boundry, 0.8, 0.47)
-#test_heterogeneous_parameters("porosity", boundry, 0.2, 0.01)
-#test_heterogeneous_parameters("permeability", boundry, 1e-13,  5.0e-13)
-#test_heterogeneous_parameters("lame_lambda", boundry, 3.3e9, 1.5e10)
-#test_heterogeneous_parameters("shear_modulus", boundry, 8.9e9, 1.5e10)
-#test_heterogeneous_parameters("friction_coefficient", boundry, 0.7, 0.65)
-#test_heterogeneous_parameters("dilation_angle", boundry, 0.1, 0.10)
-#test_heterogeneous_parameters("residual_aperture", boundry, 1e-3, 1e-3)
-#test_heterogeneous_parameters("fracture_gap", boundry, 1e-4, 1e-4)
-#test_heterogeneous_parameters("normal_permeability", boundry, 1.0e-10, 1.0e-10)
-    
+def test_lithostatic_stress():
+    ...
